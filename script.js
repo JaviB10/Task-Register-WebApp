5c4740a0505fa2dc432b6c39b233fd8f5849a22f
Javier Ballón <javi4195@gmail.com>
2024-11-14 12:21:15 -0300

    Se creo vista para users y admin

---
+++ script.js
@@ -0,0 +1,8 @@
+const sidebar = document.getElementById('sidebar')
+const menu = document.getElementById('menu')
+const main = document.getElementById('main')
+
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('menu-toggle-app')
+    main.classList.toggle('menu-toggle-app')
+})
