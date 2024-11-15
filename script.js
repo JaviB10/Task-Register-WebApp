72d1d1e58839a41e64b31d2aed3dd1de33663c90
Javier Ballón <javi4195@gmail.com>
2024-11-15 17:08:55 -0300

    Se hicieron modificaciones en estilos de la pagina

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ menu.addEventListener('click', () => {
     sidebar.classList.toggle('menu-toggle-app')
     main.classList.toggle('menu-toggle-app')
 })
+
+const menuButton = document.getElementById('menuButton')
+const menuUser = document.getElementById('menu-user')
+
+menuButton.addEventListener('click', () => {
+    menuUser.classList.toggle('hidden')
+})
